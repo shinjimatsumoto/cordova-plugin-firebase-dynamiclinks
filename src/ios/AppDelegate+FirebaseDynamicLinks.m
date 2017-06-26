39eb1ba3e6ef122c4dbde922716030ee3563c411
Maksim Chemerisuk <chemerisuk@gmail.com>
2017-06-26 11:59:55 +0300

    update delegate implementation

--- src/ios/AppDelegate+FirebaseDynamicLinks.m
+++ src/ios/AppDelegate+FirebaseDynamicLinks.m
@@ -1,99 +1,123 @@
 #import "AppDelegate+FirebaseDynamicLinks.h"
 #import "FirebaseDynamicLinks.h"
+#import <objc/runtime.h>
+
 @import Firebase;
-@import FirebaseInvites;
 @import GoogleSignIn;
 
 @implementation AppDelegate (FirebasePlugin)
 
-// [START openurl]
-- (BOOL)application:(nonnull UIApplication *)application
-            openURL:(nonnull NSURL *)url
-            options:(nonnull NSDictionary<NSString *, id> *)options {
-  FirebaseDynamicLinks* dl = [self.viewController getCommandInstance:@"FirebaseDynamicLinks"];
++ (void)load {
+    method_exchangeImplementations(
+        class_getInstanceMethod(self, @selector(application:openURL:options:)),
+        class_getInstanceMethod(self, @selector(identity_application:openURL:options:))
+    );
 
-  if ([dl isSigningIn]) {
-    dl.isSigningIn = NO;
+    method_exchangeImplementations(
+        class_getInstanceMethod(self, @selector(application:openURL:sourceApplication:annotation:)),
+        class_getInstanceMethod(self, @selector(identity_application:openURL:sourceApplication:annotation:))
+    );
 
-    return [[GIDSignIn sharedInstance] handleURL:url
-             sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
-                    annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
-  } else {
-    return [self application:application
-                     openURL:url
-           sourceApplication:options[UIApplicationOpenURLOptionsSourceApplicationKey]
-                  annotation:options[UIApplicationOpenURLOptionsAnnotationKey]];
-  }
+    method_exchangeImplementations(
+        class_getInstanceMethod(self, @selector(application:continueUserActivity:restorationHandler:)),
+        class_getInstanceMethod(self, @selector(identity_application:continueUserActivity:restorationHandler:))
+    );
 }
 
-- (BOOL)application:(UIApplication *)application
-            openURL:(NSURL *)url
-  sourceApplication:(NSString *)sourceApplication
-         annotation:(id)annotation {
-  FirebaseDynamicLinks* dl = [self.viewController getCommandInstance:@"FirebaseDynamicLinks"];
-  // Handle App Invite requests
-  FIRReceivedInvite *invite =
-      [FIRInvites handleURL:url sourceApplication:sourceApplication annotation:annotation];
-  if (invite) {
-    NSString *matchType = (invite.matchType == FIRReceivedInviteMatchTypeWeak) ? @"Weak" : @"Strong";
-    [dl sendDynamicLinkData:@{
-                             @"deepLink": invite.deepLink,
-                             @"invitationId": invite.inviteId,
-                             @"matchType": matchType
-                           }];
-    return YES;
-  }
+// [START openurl]
+- (BOOL)identity_application:(nonnull UIApplication *)application
+                     openURL:(nonnull NSURL *)url
+                     options:(nonnull NSDictionary<NSString *, id> *)options {
+    FirebaseDynamicLinks* dl = [self.viewController getCommandInstance:@"FirebaseDynamicLinks"];
+    NSString* sourceApplication = options[UIApplicationOpenURLOptionsSourceApplicationKey];
+    id annotation = options[UIApplicationOpenURLOptionsAnnotationKey];
 
-  FIRDynamicLink *dynamicLink =
-    [[FIRDynamicLinks dynamicLinks] dynamicLinkFromCustomSchemeURL:url];
-  if (dynamicLink) {
-      NSString *matchType = (dynamicLink.matchConfidence == FIRDynamicLinkMatchConfidenceWeak) ? @"Weak" : @"Strong";
-      [dl sendDynamicLinkData:@{
-                               @"deepLink": dynamicLink.url.absoluteString,
-                               @"matchType": matchType
-                             }];
+    if ([dl isSigningIn]) {
+        dl.isSigningIn = NO;
 
-      return YES;
-  }
+        return [[GIDSignIn sharedInstance] handleURL:url
+                                   sourceApplication:sourceApplication
+                                          annotation:annotation];
+    } else {
+        return [self identity_application:application
+                                  openURL:url
+                        sourceApplication:sourceApplication
+                               annotation:annotation];
+    }
+}
 
-  if ([dl isSigningIn]) {
-    dl.isSigningIn = NO;
+- (BOOL)identity_application:(UIApplication *)application
+                     openURL:(NSURL *)url
+           sourceApplication:(NSString *)sourceApplication
+                  annotation:(id)annotation {
+    FirebaseDynamicLinks* dl = [self.viewController getCommandInstance:@"FirebaseDynamicLinks"];
+    // Handle App Invite requests
+    FIRReceivedInvite *invite =
+        [FIRInvites handleURL:url sourceApplication:sourceApplication annotation:annotation];
+    if (invite) {
+        NSString *matchType = (invite.matchType == FIRReceivedInviteMatchTypeWeak) ? @"Weak" : @"Strong";
+        [dl sendDynamicLinkData:@{
+            @"deepLink": invite.deepLink,
+            @"invitationId": invite.inviteId,
+            @"matchType": matchType
+        }];
 
-    return [[GIDSignIn sharedInstance] handleURL:url
-                             sourceApplication:sourceApplication
-                                    annotation:annotation];
-  } else {
-    // call super
-    return [self application:application
-                     openURL:url
-           sourceApplication:sourceApplication
-                  annotation:annotation];
-  }
+        return YES;
+    }
+
+    FIRDynamicLink* dynamicLink =
+        [[FIRDynamicLinks dynamicLinks] dynamicLinkFromCustomSchemeURL:url];
+    if (dynamicLink) {
+        NSString* matchType = (dynamicLink.matchConfidence == FIRDynamicLinkMatchConfidenceWeak) ? @"Weak" : @"Strong";
+        [dl sendDynamicLinkData:@{
+            @"deepLink": dynamicLink.url.absoluteString,
+            @"matchType": matchType
+        }];
+
+        return YES;
+    }
+
+    if ([dl isSigningIn]) {
+        dl.isSigningIn = NO;
+
+        return [[GIDSignIn sharedInstance] handleURL:url
+                                   sourceApplication:sourceApplication
+                                          annotation:annotation];
+    } else {
+        return [self identity_application:application
+                                  openURL:url
+                        sourceApplication:sourceApplication
+                               annotation:annotation];
+    }
 }
 // [END openurl]
 
 // [START continueuseractivity]
-- (BOOL)application:(UIApplication *)application
-    continueUserActivity:(NSUserActivity *)userActivity
-      restorationHandler:(void (^)(NSArray *))restorationHandler {
+- (BOOL)identity_application:(UIApplication *)application
+        continueUserActivity:(NSUserActivity *)userActivity
+          restorationHandler:(void (^)(NSArray *))restorationHandler {
     FirebaseDynamicLinks* dl = [self.viewController getCommandInstance:@"FirebaseDynamicLinks"];
 
     BOOL handled = [[FIRDynamicLinks dynamicLinks]
-                     handleUniversalLink:userActivity.webpageURL
-                              completion:^(FIRDynamicLink * _Nullable dynamicLink,
-                                           NSError * _Nullable error) {
+        handleUniversalLink:userActivity.webpageURL
+        completion:^(FIRDynamicLink * _Nullable dynamicLink, NSError * _Nullable error) {
+            if (dynamicLink) {
+                NSString *matchType = (dynamicLink.matchConfidence == FIRDynamicLinkMatchConfidenceWeak) ? @"Weak" : @"Strong";
 
-      if (dynamicLink) {
-        NSString *matchType = (dynamicLink.matchConfidence == FIRDynamicLinkMatchConfidenceWeak) ? @"Weak" : @"Strong";
+                [dl sendDynamicLinkData:@{
+                    @"deepLink": dynamicLink.url.absoluteString,
+                    @"matchType": matchType
+                }];
+            }
+        }];
 
-        [dl sendDynamicLinkData:@{
-           @"deepLink": dynamicLink.url.absoluteString,
-           @"matchType": matchType
-         }];
-      }
-  }];
-
-  return handled;
+    if (handled) {
+        return YES;
+    } else {
+        return [self identity_application:application
+                     continueUserActivity:userActivity
+                       restorationHandler:restorationHandler];
+    }
 }
 // [END continueuseractivity]
 
