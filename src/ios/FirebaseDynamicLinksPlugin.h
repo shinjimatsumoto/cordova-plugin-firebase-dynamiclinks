71cd08572d9913d2013d082368295f10cfe46754
dmais <dmai@knil.co>
2017-07-26 12:58:33 +0300

    removed invite from iOS

--- src/ios/FirebaseDynamicLinksPlugin.h
+++ src/ios/FirebaseDynamicLinksPlugin.h
@@ -4,11 +4,9 @@
 @import Firebase;
 @import GoogleSignIn;
 
-@interface FirebaseDynamicLinksPlugin : CDVPlugin<FIRInviteDelegate, GIDSignInDelegate, GIDSignInUIDelegate>
+@interface FirebaseDynamicLinksPlugin : CDVPlugin<GIDSignInDelegate, GIDSignInUIDelegate>
 
 - (void)onDynamicLink:(CDVInvokedUrlCommand *)command;
-- (void)sendInvitation:(CDVInvokedUrlCommand*)command;
-- (void)postDynamicLink:(NSString*) deepLink weakConfidence:(BOOL) weakConfidence inviteId:(NSString*) inviteId;
 
 @property (nonatomic, copy) NSString *dynamicLinkCallbackId;
 @property (nonatomic, assign) BOOL isSigningIn;
