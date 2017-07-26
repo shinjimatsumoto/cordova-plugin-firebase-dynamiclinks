27dc41ee63cc8531f6466513619341099711d14e
dmais <dmai@knil.co>
2017-07-26 13:47:36 +0300

    reverted to original FirebaseDynamicLinksPlugin.h

--- src/ios/FirebaseDynamicLinksPlugin.h
+++ src/ios/FirebaseDynamicLinksPlugin.h
@@ -4,9 +4,11 @@
 @import Firebase;
 @import GoogleSignIn;
 
-@interface FirebaseDynamicLinksPlugin : CDVPlugin<GIDSignInDelegate, GIDSignInUIDelegate>
+@interface FirebaseDynamicLinksPlugin : CDVPlugin<FIRInviteDelegate, GIDSignInDelegate, GIDSignInUIDelegate>
 
 - (void)onDynamicLink:(CDVInvokedUrlCommand *)command;
+- (void)sendInvitation:(CDVInvokedUrlCommand*)command;
+- (void)postDynamicLink:(NSString*) deepLink weakConfidence:(BOOL) weakConfidence inviteId:(NSString*) inviteId;
 
 @property (nonatomic, copy) NSString *dynamicLinkCallbackId;
 @property (nonatomic, assign) BOOL isSigningIn;
