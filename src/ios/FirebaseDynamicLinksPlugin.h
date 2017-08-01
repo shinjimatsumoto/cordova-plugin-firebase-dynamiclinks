7491bd2c7ee8f0815cad6174fc56b2e5281e7405
dmais <dmai@knil.co>
2017-08-01 14:22:08 +0300

    Update FirebaseDynamicLinksPlugin.h

--- src/ios/FirebaseDynamicLinksPlugin.h
+++ src/ios/FirebaseDynamicLinksPlugin.h
@@ -4,7 +4,7 @@
 @import Firebase;
 @import GoogleSignIn;
 
-@interface FirebaseDynamicLinksPlugin : CDVPlugin<FIRInviteDelegate, GIDSignInDelegate, GIDSignInUIDelegate>
+@interface FirebaseDynamicLinksPlugin : CDVPlugin
 
 - (void)onDynamicLink:(CDVInvokedUrlCommand *)command;
 - (void)sendInvitation:(CDVInvokedUrlCommand*)command;
